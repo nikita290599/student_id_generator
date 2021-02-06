5adf3696b02446a355dda5e7f7b3516765a087ec
Nikita satnami <2018168@iiitdmj.ac.in>
2021-02-06 13:25:16 +0530

    1st version

---
+++ js/index.js
@@ -0,0 +1,37 @@
+
+
+
+
+ function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      $('#blah').attr('src', e.target.result);
+    }
+
+    reader.readAsDataURL(input.files[0]); // convert to base64 string
+  }
+}
+
+$("#imageInput").change(function() {
+  readURL(this);
+});
+
+$('button').on('click', function () {
+    let name = $("#name").val();
+    let location = $("#location").val();
+    let college = $("#college").val();
+    // alert(name);
+    // $("#idCard").addClass('idClass');
+    let src= $("#blah").attr('src');
+    $(".idImage").css('background-image', "url(" + src + ")");
+    
+
+    $(".idContent").html("<b>Name:</b>  "+ name+"<br><br><b>College:</b> "+ college+"<br><br><b>Location:  </b>"+ location );
+    
+
+    return false;
+
+});
+
